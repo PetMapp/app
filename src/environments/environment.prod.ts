81c19bfd363eac4d2c7491eb5f26a9d1b34ba961
Eduardo Oliveira <48637125+eduardo14rj@users.noreply.github.com>
2024-10-10 08:04:32 -0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiKey: "AIzaSyDbIP8cBMs5YKb9lQIPeDSXBP7Z5AOpI_4",
-  ApiBaseUrl: "https://api-bs6hj767iq-uc.a.run.app/",
+  ApiBaseUrl: "https://api-bs6hj767iq-uc.a.run.app/api/",
   firebaseConfig: {
     apiKey: "AIzaSyBqAgj1i9CeDOSLvrmzoyjQAFlomWMyX0s",
     authDomain: "petmap-6d5f7.firebaseapp.com",
