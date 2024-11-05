3f3e59874180c8380bca8e30d85d1bd670dbe048
gustahh <gustavocarlos206@gmail.com>
2024-11-05 18:56:33 -0300

    lista de pets do usuário

--- src/app/components/pet/pet.component.ts
+++ src/app/components/pet/pet.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { PetsService } from 'src/app/services/pets.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pet',
   templateUrl: './pet.component.html',
   styleUrls: ['./pet.component.scss'],
 })
-export class PetComponent  implements OnInit {
+export class PetComponent implements OnInit {
 
-  constructor() { }
+  pets: any[] = [];
 
-  ngOnInit() {}
+  constructor(private petsService: PetsService, private nav: NavController) {}
 
+  ngOnInit(): void {
+    this.petsService.getPets().subscribe({
+      next: (pets) => {
+        this.pets = pets;
+      },
+      error: (error) => {
+        console.error("Erro ao carregar pets:", error);
+      }
+    });
+  }
+
+  goToDetails(petId: string) {
+    this.nav.navigateForward(['pet-details'], { state: { petId } });
+  }
 }
